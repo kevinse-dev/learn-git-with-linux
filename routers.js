be883efa58346b8c7e389b23c6469632bf23aab5
KAVIN SEBASTIAN <kavin.sebastian25@gmail.com>
2022-03-29 15:00:08 +0700

    Create endpoint for user detail and add user

--- routers.js
+++ routers.js
@@ -1,9 +1,30 @@
 const router = require('express').Router()
+const { v4: uuidv4 } = require('uuid');
 
+const users = []
 
 router.get('/', (req, res) => {
-    res.send('Hello wolrd')
+    res.send('Hello world')
 })
 
+router.get('/users', (req, res) => {
+    res.send(users)
+})
+
+router.post('/users', (req, res) => {
+    const user = req.body
+    try {
+        users.push({...user, id:uuidv4()})
+        res.json({
+            status:201,
+            message:`Add user with name ${user.firstname} success in database`
+        })
+    } catch (error) {
+        res.json({
+            status:401,
+            message:error
+        })
+    }
+})
 
 module.exports = router
